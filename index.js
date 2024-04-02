a64893f0cee943e81de09f09480a154b71ec4d95
hye1ee <hyewon0809@kaist.ac.kr>
2024-04-02 19:02:19 -0400

    [feat] Implement restyle function

--- index.js
+++ index.js
@@ -73,18 +73,54 @@ const names = [
   "Ancalagon",
 ];
 
+const colorvals = [
+  [242, 76, 0],
+  [72, 86, 150],
+  [252, 122, 30],
+  [51, 0, 54],
+  [10, 54, 157],
+  [94, 124, 226],
+  [136, 102, 93],
+  [142, 85, 114],
+  [196, 158, 133],
+  [228, 154, 176],
+  [205, 231, 127],
+  [255, 240, 124],
+  [62, 255, 139],
+  [56, 108, 11],
+];
+
+const getRandomIdx = (max) => {
+  return Math.floor(Math.random() * max);
+};
+
 const generator = (input) => {
-  const hashValues = [0, 0, 0];
+  const hashVals = [0, 0, 0];
   for (let i = 0; i < input.length; i++) {
-    hashValues[i % 3] += input.charCodeAt(i);
+    hashVals[i % 3] += input.charCodeAt(i);
   }
-  return `${colors[hashValues[0] % colors.length]} ${
-    adjectives[hashValues[1] % adjectives.length]
-  } ${names[hashValues[2] % names.length]}`;
+  return `${colors[hashVals[0] % colors.length]} ${
+    adjectives[hashVals[1] % adjectives.length]
+  } ${names[hashVals[2] % names.length]}`;
+};
+
+const restyle = () => {
+  const resultText = document.getElementById("div-result");
+  resultText.className = `font-${getRandomIdx(6) + 1}`;
+
+  const colorIdx = getRandomIdx(colorvals.length);
+  resultText.style.color = `rgb(${colorvals[colorIdx][0]},${colorvals[colorIdx][1]},${colorvals[colorIdx][2]})`;
+
+  const fontSize = 20 + getRandomIdx(50);
+  resultText.style.fontSize = `${fontSize}px`;
+
+  const rotationDeg = getRandomIdx(180) - 90;
+  resultText.style.transform = `rotate(${rotationDeg}deg)`;
 };
 
 document.getElementById("button-submit").addEventListener("click", () => {
   document.getElementById("div-result").innerText = generator(
     document.getElementById("input-name").value
   );
+  restyle();
 });
